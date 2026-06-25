757f9bb432fa4b4984b22fcbae696770e62fbea4
VinayakTripathi427 <vinayaktripathi4267@gmail.com>
2026-06-25 12:25:25 +0530

    Add files via upload

---
+++ Classwork/Selection statement/Average.py
@@ -0,0 +1,14 @@
+#Average of 5 Numbers
+
+#input from User
+num1 = float(input("Enter first Number: "))
+num2 = float(input("Enter second Number: "))
+num3 = float(input("Enter third Number: "))
+num4 = float(input("Enter fourth Number: "))
+num5 = float(input("Enter fifth Number: "))
+
+#Calculation
+Average = (num1+num2+num3+num4+num5)/5
+
+#Output
+print("Average of Numbers: ",Average)
