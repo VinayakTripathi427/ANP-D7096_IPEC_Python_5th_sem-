711c6d0df4a8c861cc7a3e373042123f72efe716
VinayakTripathi427 <vinayaktripathi4267@gmail.com>
2026-06-26 11:19:53 +0530

    Checking Natural numbers.

---
+++ Classwork/If else statement_26th_june/NaturalNumber Checker.py
@@ -0,0 +1,21 @@
+'''Program to check wheather given number is Natural Number or not.
+-----------------------------------------------------------------'''
+
+#Input from the user
+Num = int(input("Enter a Number: "))
+
+#Checking Wheather given number is Natural or not
+#---------------------------------------------
+
+
+if(Num<=0):
+    exit("Given Number is not Natural.")
+
+#---------------------------------------------
+    
+else:
+    print("Given Number is Natural.")
+
+'''---------------------------------------------
+----------------------------------------------'''
+    
